dcb8bb15d8036b699d1653455a7ebf1c9f4058c8
Aidan <admin@MBP.local>
2014-09-29 20:57:14 +0800

    Initial Commit

---
+++ CardGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardGame
+//
+//  Created by Aidan on 14-9-29.
+//  Copyright (c) 2014年 ideal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CardGame/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CardGame
+//
+//  Created by Aidan on 14-9-29.
+//  Copyright (c) 2014年 ideal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
